f4e649811380f451ee7b84bd970fcdc4fc59e3f2
Videog4mer <jhuse01@gmail.com>
2015-11-18 23:14:52 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The Hub MC - Home</title>
+<meta property='og:site_name' content='The Hub MC' />
+<meta property='og:title' content='The Hub MC' />
+<meta property='og:description' content='Welcome to our website home page! We have made things just for our players! We have installed a Forums software, called XenForo, made a Economy Stats Page , and a Bending Stats Page . If you have any...' />
+<meta property='og:image' content='http://the-hub-mc.com/uploads/3/7/4/8/3748953/285050484.png' />
+<meta property='og:url' content='http://the-hub-mc.com/' />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<link rel="stylesheet" href="//assets.www3.dragndropbuilder.com/css/sites.css?buildTime=1432850887" type="text/css" /><link rel='stylesheet' type='text/css' href='http://www3.dragndropbuilder.com/editor/libraries/fancybox/fancybox.css?1432850887' />
+<link rel='stylesheet' type='text/css' href='/files/main_style.css?1444944780' title='wsite-theme-css' />
+<link href='http://fonts.googleapis.com/css?family=Actor' rel='stylesheet' type='text/css' />
+<style type='text/css'>
+.wsite-elements div.paragraph, .wsite-elements p, .wsite-elements .product-block .product-title, .wsite-elements .product-description, .wsite-elements .wsite-form-field label, .wsite-elements .wsite-form-field label, #wsite-content div.paragraph, #wsite-content p, #wsite-content .product-block .product-title, #wsite-content .product-description, #wsite-content .wsite-form-field label, #wsite-content .wsite-form-field label, .blog-sidebar div.paragraph, .blog-sidebar p, .blog-sidebar .wsite-form-field label, .blog-sidebar .wsite-form-field label {}
+#wsite-content div.paragraph, #wsite-content p, #wsite-content .product-block .product-title, #wsite-content .product-description, #wsite-content .wsite-form-field label, #wsite-content .wsite-form-field label, .blog-sidebar div.paragraph, .blog-sidebar p, .blog-sidebar .wsite-form-field label, .blog-sidebar .wsite-form-field label {}
+.wsite-elements h2, .wsite-elements .product-long .product-title, .wsite-elements .product-large .product-title, .wsite-elements .product-small .product-title, #wsite-content h2, #wsite-content .product-long .product-title, #wsite-content .product-large .product-title, #wsite-content .product-small .product-title, .blog-sidebar h2 {}
+#wsite-content h2, #wsite-content .product-long .product-title, #wsite-content .product-large .product-title, #wsite-content .product-small .product-title, .blog-sidebar h2 {}
+#wsite-title {}
+</style>
+<style type='text/css'>
+.wsite-header {
+	background-image: url(/uploads/3/7/4/8/3748953/header_images/1444884011.jpg) !important;
+	background-position: 0 0 !important;
+}
+</style>
+<script><!--
+var STATIC_BASE = 'http://www3.dragndropbuilder.com/';
+var STYLE_PREFIX = 'wsite';
+//-->
+</script>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+<script src='//assets.www3.dragndropbuilder.com/js/site/main.js?buildTime=1432850887'></script>
+<script type='text/javascript'><!--
+(function(jQuery){
+function initFlyouts(){initPublishedFlyoutMenus([{"id":"637816215436028582","title":"Home","url":"index.html","target":""},{"id":"187150172419460318","title":"Forums","url":"http:\/\/the-hub-mc.com\/forum\/articles\/","target":"_blank"},{"id":"734199010485427648","title":"Bending Stats","url":"http:\/\/the-hub-mc.com\/bending\/","target":"_blank"},{"id":"628446890623804656","title":"Economy Stats","url":"http:\/\/the-hub-mc.com\/economy\/","target":"_blank"},{"id":"558192844871952731","title":"Wiki","url":"http:\/\/the-hub-mc.com\/forum\/wiki\/index.php","target":"_blank"},{"id":"927341342582140390","title":"Bans","url":"http:\/\/the-hub-mc.com\/bans\/","target":"_blank"}],'637816215436028582',"<li class='wsite-nav-more'><a href='#'>more...<\/a><\/li>",'active',false)}
+if (jQuery) {
+if (jQuery.browser.msie && !window.flyoutMenusRefreshable) window.onload = initFlyouts;
+else jQuery(initFlyouts);
+}else{
+if (Prototype.Browser.IE) window.onload = initFlyouts;
+else document.observe('dom:loaded', initFlyouts);
+}
+})(window._W && _W.jQuery)
+//-->
+</script>
+</head>
+<body class='wsite-theme-light landing-page wsite-page-index'>
+<div id="header-wrap">
+	<div id="page">
+		<table id="header">
+			<tr>
+				<td id="logo"><span class='wsite-logo'><a href='/'><img src='/uploads/3/7/4/8/3748953/285050484.png' /></a></span></td>
+				<td id="header-right">
+					<table>
+						<tr>
+							<td class="phone-number"><span class='wsite-text'>IP: the-hub-mc.net</span></td>
+							<td class="social"><div style="text-align:left;"><div style="height:0px;overflow:hidden"></div>
+
+<span class="wsite-social wsite-social-default"><a class='wsite-social-item wsite-social-twitter' href='http://twitter.com/The_Hub_MC' target='_blank'><span class='wsite-social-item-inner'></span></a><a class='wsite-social-item wsite-social-mail' href='mailto:jhuse01@gmail.com' target='_blank'><span class='wsite-social-item-inner'></span></a></span>
+
+<div style="height:0px;overflow:hidden"></div></div></td>
+						</tr>
+					</table>
+					<div class="search"></div>
+				</td>
+			</tr>
+		</table>
+		<div id="topnav">
+			<ul class='wsite-menu-default'><li id='active'><a href='/index.html'>Home</a></li><li id='pg187150172419460318'><a href='http://the-hub-mc.com/forum/articles/' target='_blank'>Forums</a></li><li id='pg734199010485427648'><a href='http://the-hub-mc.com/bending/' target='_blank'>Bending Stats</a></li><li id='pg628446890623804656'><a href='http://the-hub-mc.com/economy/' target='_blank'>Economy Stats</a></li><li id='pg558192844871952731'><a href='http://the-hub-mc.com/forum/wiki/index.php' target='_blank'>Wiki</a></li><li id='pg927341342582140390'><a href='http://the-hub-mc.com/bans/' target='_blank'>Bans</a></li></ul>
+			<div style="clear:both"></div>
+		</div>
+	</div>
+</div>
+<div id="banner-wrap">
+	<div id="container">
+		<div id="banner">
+			<div id="bannerleft">
+				<div id="banner-b">
+					<div id="banner-t">
+						<div id="banner-m">
+							<div id="banner-inner">
+								<div class="wsite-header"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="bannerright" class="landing-banner-outer">
+				<div class="landing-banner-mid">
+					<div class="landing-banner-inner">
+						<h2><span class='wsite-text'>News and Announcements!</span></h2>
+						<p><span class='wsite-text'>Get the latest News and Announcements on our forums page!</span></p>
+						<div style="text-align:center;"><div style="height: 0px; overflow: hidden;"></div>
+
+<a class="wsite-button wsite-button-large wsite-button-highlight" href="http://the-hub-mc.com/forum/articles" target="_blank">
+
+<span class="wsite-button-inner">News and Announcements</span>
+
+</a>
+
+<div style="height: 0px; overflow: hidden;"></div></div>
+					</div><!-- end banner inner -->
+				</div><!-- end banner mid -->
+			</div><!-- end banner-right -->
+			<div style="clear:both;"></div>
+		</div>
+	</div>
+</div>
+<div id="main-wrap">
+	<div id="page">
+		<div id="main">
+			<div id="content"><div id='wsite-content' class='wsite-elements wsite-not-footer'>
+<h2 style="text-align:left;">Welcome to our website!</h2>
+
+<div class="paragraph" style="text-align:left;">Welcome to our website home page! We have made things just for our players! We have installed a <strong>Forums </strong>software, called XenForo, made a <strong>Economy Stats Page</strong>, and a <strong>Bending Stats Page</strong>. If you have any suggestions, please post them on our forums in our "Suggestions" category!<br /><br />- Jackson</div></div>
+</div>
+		</div>
+	</div>
+</div>
+<div id="footer-wrap">
+	<div id="page">
+		<div id="footer"><div class='wsite-elements wsite-footer'>
+<div><div class="wsite-multicol"><div class='wsite-multicol-table-wrap' style='margin:0 -15px'>
+<table class='wsite-multicol-table'>
+<tbody class='wsite-multicol-tbody'>
+<tr class='wsite-multicol-tr'>
+<td class='wsite-multicol-col' style='width:50%;padding:0 15px'></td>
+<td class='wsite-multicol-col' style='width:50%;padding:0 15px'></td>
+</tr>
+</tbody>
+</table>
+</div></div></div>
+
+<div class="paragraph"><a href='#' target='_blank'>The Hub MC</a> Website</div></div><script type='text/javascript'>
+<!--
+
+if (document.cookie.match(/(^|;)\s*is_mobile=1/)) {
+	var windowHref = window.location.href || '';
+	if (windowHref.indexOf('?') > -1) {
+		windowHref += '&';
+	} else {
+		windowHref += '?';
+	}
+	document.write(
+		"&nbsp;&nbsp;&nbsp;&nbsp;" +
+		"<a class='wsite-view-link-mobile' href='" + windowHref + "view=mobile'>Mobile Site</a>"
+	);
+}
+
+//-->
+</script>
+</div>
+	</div>
+</div>
+
+</body>
+							
+</html>
